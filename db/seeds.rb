9812682528a596d68ef8fa30317b38e876b335f2
doctor3397 <hs2546@columbia.edu>
2017-02-11 15:03:22 -0500

    Add new restaurants in seeds and add pictures in assets/images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,42 +5,68 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.destroy_all
+
 Restaurant.create!(
-  name: "Kal and Mooy",
+  name: "Canyon Creek",
   city: "Richmond Hill",
   capcity: "18",
-  cuisine: "African",
-  image: "1.png"
-  )
+  cuisine: "Steakhouse",
+  image: "steak.jpg"
+)
 
-  Restaurant.create!(
-    name: "Me-N-U",
-    city: "Queen West",
-    capcity: "24",
-    cuisine: "Asian Fusion",
-    image: "asian_foodtruck2.png"
-  )
+Restaurant.create!(
+  name: "The Chase",
+  city: "Queen West",
+  capcity: "24",
+  cuisine: "American",
+  image: "hamburger.jpg"
+)
 
-      Restaurant.create!(
-        name: "Blue Donkey",
-        city: "The Beaches",
-        capcity: "30",
-        cuisine: "Greek",
-        image: "blue_donkey_foodtruck1.png"
-      )
+Restaurant.create!(
+  name: "Campagnolo",
+  city: "The Beaches",
+  capcity: "30",
+  cuisine: "Italian",
+  image: "pasta.jpg"
+)
 
-      Restaurant.create!(
-        name: "Feisty Jack",
-        city: "Etobicoke",
-        capcity: "33",
-        cuisine: "English",
-        image: "churros_foodtruck6.png"
-      )
+Restaurant.create!(
+  name: "Mamakas",
+  city: "Etobicoke",
+  capcity: "33",
+  cuisine: "Greek",
+  image: "hummus.jpg"
+)
 
-      Restaurant.create!(
-        name: "Randy's Curbside",
-        city: "North York",
-        capcity: "14",
-        cuisine: "BBQ and Grill",
-        image: "cupcakes_foodtruck2.png"
-      )
+Restaurant.create!(
+  name: "Earth to Table",
+  city: "North York",
+  capcity: "14",
+  cuisine: "Pizzeria",
+  image: "pizza.jpg"
+)
+
+Restaurant.create!(
+  name: "Cafe Boulud",
+  city: "The Annex",
+  capcity: "14",
+  cuisine: "French",
+  image: "risotto.jpg"
+)
+
+Restaurant.create!(
+  name: "Miku Izakaya",
+  city: "The Annex",
+  capcity: "14",
+  cuisine: "Japanese",
+  image: "niku.jpg"
+)
+
+Restaurant.create!(
+  name: "Lee Chen Asian Bitro",
+  city: "King West",
+  capcity: "14",
+  cuisine: "Chinese",
+  image: "chinese.jpg"
+)
