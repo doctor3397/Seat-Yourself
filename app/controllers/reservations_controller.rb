b5c8e4d24f38892d6fa6aba739070276269657d7
doctor3397 <hs2546@columbia.edu>
2017-02-12 12:35:44 -0500

    Fix the Reservation save error

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -13,21 +13,21 @@ class ReservationsController < ApplicationController
 
   def create
     reservation = Reservation.new(reservation_params)
-    user = User.find(session[:user_id])
-    restaurant = Restaurant.find(params[:reservation][:restaurant_id])
+    reservation.user = User.find(session[:user_id])
+    #restaurant = Restaurant.find(params[:reservation][:restaurant_id])
 
-    if reservation.party_size < restaurant.remain_seat_search(params[:reservation][:date], params[:reservation][:res_time])
+    # if reservation.party_size < restaurant.remain_seat_search(params[:reservation][:date], params[:reservation][:res_time])
 
       if reservation.save
-        redirect_to user_path(user)
+        redirect_to user_path(reservation.user)
       else
         redirect_to restaurant_path(params[:reservation][:restaurant_id])
       end
 
-    else
-      flash[:error] = "Sorry, we don't have enough seats for #{reservation.party_size}"
-      redirect_to restaurant_path(params[:reservation][:restaurant_id])
-    end
+    # else
+    #   flash[:error] = "Sorry, we don't have enough seats for #{reservation.party_size}"
+    #   redirect_to restaurant_path(params[:reservation][:restaurant_id])
+    # end
 
   end
 
