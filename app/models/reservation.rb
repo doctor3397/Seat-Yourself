7e79d39381128162c9d44f64c8ee2149b43901e7
doctor3397 <hs2546@columbia.edu>
2017-02-12 11:32:52 -0500

    Print errors.full_messages to form new/edit

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
-  validates :res_time, :date, :party_size, :restaurant_id, :user_id presence: true
+  validates :res_time, :date, :party_size, :restaurant_id, :user_id, presence: true
 end
